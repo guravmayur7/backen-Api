e5fc738497164c6b2a01ab08d316b51d52b8fc4b
Mayur Gurav <guravmayur7@gmail.com>
2023-03-13 16:39:04 +0530

    init setup

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
-const app = require("./app");
+import express from "express";
+const app = express();
 
+// #############################################################################
+// Logs all request paths and method
+
+// #############################################################################
+// This configures static hosting for files in /public that have the extensions
+// listed in the array.
+
+// #############################################################################
+// Catch all handler for all other request.
+
+app.get("/demo", (request, response) => {
+  response.send(" demo app is working");
+});
+
+app.get("/", (request, response) => {
+  response.send("app is working");
+});
 const port = process.env.PORT || 5000;
-
 app.listen(port);
