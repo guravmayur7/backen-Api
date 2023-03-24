c689b667061cdb58f5af3530d84699f6beff3fa0
Mayur Gurav <guravmayur7@gmail.com>
2023-03-24 13:57:42 +0530

    initial update

--- server.js
+++ server.js
@@ -1,16 +1,6 @@
 import express from "express";
 const app = express();
 
-// #############################################################################
-// Logs all request paths and method
-
-// #############################################################################
-// This configures static hosting for files in /public that have the extensions
-// listed in the array.
-
-// #############################################################################
-// Catch all handler for all other request.
-
 app.get("/demo", (request, response) => {
   response.send(" demo app is working");
 });
