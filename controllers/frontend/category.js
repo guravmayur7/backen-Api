179ea56f9793409b76ae4b9d07cb27751864c331
Mayur PC <guravmayur7@gmail.com>
2023-05-17 12:28:43 +0530

    category structure api update

--- controllers/frontend/category.js
+++ controllers/frontend/category.js
@@ -17,17 +17,26 @@ export const getCategory = async (req, res) => {
     //   //     },
     //   //   },
     // ])
-    await Category.find()
+    await Category.find({ parent_id: null })
       .then(async (result) => {
         if (result) {
           let responseArray = [];
           for (let i = 0; i < result.length; i++) {
             //check here repeat category id
-            let parent = result[i];
+            let parent = result[i]; //women
             let child = {};
-            child = await getChildCategory(parent._id.toString());
+            child = await getChildCategory(parent._id.toString()); //athenic
+            if (child.length > 0) {
+              for (let c = 0; c < child.length; c++) {
+                //const element = child[c];
+                let gChild = {};
+                gChild = await getChildCategory(child[c]._id.toString());
+                child = { parentCat: child[c], childCat: gChild };
+              }
+            }
             responseArray[i] = { parentCat: parent, childCat: child };
           }
+
           return await res.status(200).send(responseArray);
         }
       })
